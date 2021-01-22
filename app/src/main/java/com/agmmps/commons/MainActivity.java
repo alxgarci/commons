b7a77855f517e0bae3aeebfe638a8e67c9ab6531
pablosc93 <pablo29.sanchezc@gmail.com>
2021-01-22 13:33:57 +0100

    prueba log in

--- app/src/main/java/com/agmmps/commons/MainActivity.java
+++ app/src/main/java/com/agmmps/commons/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ke pasa brode
+    //prueba login
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
