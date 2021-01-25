ad39c8f0dd855fd051c762768426dda81b02855a
Alex García <alejandrogarci2000@gmail.com>
2021-01-25 14:05:27 +0100

    splash glide

--- app/src/main/java/com/agmmps/commons/SplashActivity.java
+++ app/src/main/java/com/agmmps/commons/SplashActivity.java
@@ -3,12 +3,28 @@ package com.agmmps.commons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
+    TextView nombreApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.ivLogoSplash);
+        nombreApp = findViewById(R.id.tvNombreAppSplash);
+
+        Glide.with(this)
+                .load(R.drawable.ic_logo_usuarios)
+                .fitCenter()
+                .transition(DrawableTransitionOptions.withCrossFade(500));
     }
 }
