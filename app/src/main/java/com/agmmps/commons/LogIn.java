a1e2230d8c7eb85623161cc66fcb5a5f5f995ae2
pablosc93 <pablo29.sanchezc@gmail.com>
2021-01-25 22:43:50 +0100

    log in basico

--- app/src/main/java/com/agmmps/commons/LogIn.java
+++ app/src/main/java/com/agmmps/commons/LogIn.java
@@ -2,13 +2,55 @@ package com.agmmps.commons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
+
+    EditText etUsuario;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        etUsuario = findViewById(R.id.etLoginUsuario);
+        etPassword = findViewById(R.id.etLoginPassword);
+
+    }
+
+    public void acceder(View view) {
+
+        String usuario = etUsuario.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (usuario.isEmpty()) {
+            //Toast
+        } else if (password.isEmpty()) {
+            //Toast
+        } else {
+            int num = 5;
+            /*
+            creamos el objeto Usuario
+            consulta usuario en la BD
+
+            if (usuario existe en la BD) {
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+            } else {
+                //Toast
+            }
+            */
+
+        }
+
+    }
+
+    public void abrirSignup(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
