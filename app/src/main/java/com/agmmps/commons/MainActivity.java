ae166a4d65b8b0926317417cb21c63c2e4c01996
pablosc93 <pablo29.sanchezc@gmail.com>
2021-01-25 13:37:57 +0100

    lpjnpk

--- app/src/main/java/com/agmmps/commons/MainActivity.java
+++ app/src/main/java/com/agmmps/commons/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ke pasa brode
+    //ke 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
