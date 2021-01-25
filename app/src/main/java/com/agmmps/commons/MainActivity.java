94efdae9f4960af3336ee09ea05ad9e3c0c386bb
pablosc93 <pablo29.sanchezc@gmail.com>
2021-01-25 13:20:51 +0100

    sgnnnnn

--- app/src/main/java/com/agmmps/commons/MainActivity.java
+++ app/src/main/java/com/agmmps/commons/MainActivity.java
@@ -6,8 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //gjtgjtjtjt
-
+    //llllllllllllllllllllllllll
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
