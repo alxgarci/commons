e7c6244cfa07a1510b94b428539a13a2a69242b2
Alex García <alejandrogarci2000@gmail.com>
2021-01-27 16:44:51 +0100

    SplashActivity.java terminado

--- app/src/main/java/com/agmmps/commons/SplashActivity.java
+++ app/src/main/java/com/agmmps/commons/SplashActivity.java
@@ -17,6 +17,7 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SplashActivity extends AppCompatActivity {
 
+    //s
     ImageView logo;
     TextView nombreApp;
 
