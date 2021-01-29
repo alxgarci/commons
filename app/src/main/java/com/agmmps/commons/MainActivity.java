8d3f36de39a5e1eebb316e6ae4a9dffa6f0fdf0b
pablosc93 <pablo29.sanchezc@gmail.com>
2021-01-29 13:45:52 +0100

    InicioFragment version 1

--- app/src/main/java/com/agmmps/commons/MainActivity.java
+++ app/src/main/java/com/agmmps/commons/MainActivity.java
@@ -1,16 +1,39 @@
 package com.agmmps.commons;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.agmmps.commons.fragments.InicioFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     //gjtgjtjtjtkkk
 
+    LinearLayout ll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll = findViewById(R.id.llMainContenedor);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        InicioFragment iFrag = InicioFragment.newInstance();
+//        Bundle bundle = new Bundle();
+//        bundle.putString(NAME_KEY, nom);
+//        cf.setArguments(bundle);
+
+        ft.add(R.id.llMainContenedor, iFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+
+
     }
 }
---
+++ app/src/main/java/com/agmmps/commons/fragments/InicioFragment.java
@@ -0,0 +1,59 @@
+package com.agmmps.commons.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agmmps.commons.R;
+
+/*
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InicioFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InicioFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public InicioFragment() {
+        // Required empty public constructor
+    }
+
+    public static InicioFragment newInstance() {
+        InicioFragment fragment = new InicioFragment();
+        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+
+        return view;
+    }
+}
