f564ace42f4866a6ccaf69a89197491bf2f705ac
ccapu <c.capuchino@hotmail.com>
2021-02-05 12:45:00 +0100

    loa

--- app/src/main/java/com/agmmps/commons/MainActivity.java
+++ app/src/main/java/com/agmmps/commons/MainActivity.java
@@ -1,10 +1,17 @@
 package com.agmmps.commons;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.agmmps.commons.fragments.PerfilFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     //gjtgjtjtjt
 
@@ -12,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
