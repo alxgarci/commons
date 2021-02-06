6a8b39f0d31a6be3361cab06cc75bd06d2ea7cde
pablosc93 <pablo29.sanchezc@gmail.com>
2021-02-06 12:50:52 +0100

    hh

--- app/src/main/java/com/agmmps/commons/javabeans/Datos.java
+++ app/src/main/java/com/agmmps/commons/javabeans/Datos.java
@@ -10,7 +10,7 @@ public class Datos {
 
     public Datos() {
         datos = new ArrayList<Anuncio>();
-        //f
+        //fg
         datos.add(new Anuncio(1, "Busco amiguitos para salir por ahi", (new Usuario(1, R.drawable.usuario_1, "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
         datos.add(new Anuncio(2, "Busco amiguitas para jugar al ping pong", (new Usuario(2, R.drawable.usuario_2, "Mikel Izubirraga", "Bernabeu", "Soy una chico muy enrollado"))));
         datos.add(new Anuncio(3, "Busco amiguitos para salir por ahi", (new Usuario(3, R.drawable.usuario_3, "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
