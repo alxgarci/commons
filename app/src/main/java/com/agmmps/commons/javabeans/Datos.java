871b7e2502992134684438112109b9e1f7d67e5e
pablosc93 <pablo29.sanchezc@gmail.com>
2021-02-06 12:50:20 +0100

    rt

--- app/src/main/java/com/agmmps/commons/javabeans/Datos.java
+++ app/src/main/java/com/agmmps/commons/javabeans/Datos.java
@@ -13,8 +13,8 @@ public class Datos {
         //f
         datos.add(new Anuncio(1, "Busco amiguitos para salir por ahi", (new Usuario(1, R.drawable.usuario_1, "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
         datos.add(new Anuncio(2, "Busco amiguitas para jugar al ping pong", (new Usuario(2, R.drawable.usuario_2, "Mikel Izubirraga", "Bernabeu", "Soy una chico muy enrollado"))));
-        datos.add(new Anuncio(1, "Busco amiguitos para salir por ahi", (new Usuario(3, R.drawable.usuario_3, "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
-        datos.add(new Anuncio(2, "Busco amiguitas para jugar al ping pong", (new Usuario(4, R.drawable.usuario_4, "Mikel Izubirraga", "Bernabeu", "Soy una chico muy enrollado"))));
+        datos.add(new Anuncio(3, "Busco amiguitos para salir por ahi", (new Usuario(3, R.drawable.usuario_3, "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
+        datos.add(new Anuncio(4, "Busco amiguitas para jugar al ping pong", (new Usuario(4, R.drawable.usuario_4, "Mikel Izubirraga", "Bernabeu", "Soy una chico muy enrollado"))));
 
     }
 
