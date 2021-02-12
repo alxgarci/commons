624fce8685c0f9f3f7409d22b87edf952e33ed8a
pablosc93 <pablo29.sanchezc@gmail.com>
2021-02-12 10:16:06 +0100

    ajustes y editperf Fragments2

---
+++ app/src/main/java/com/agmmps/commons/fragments/AnuncioFragment.java
@@ -0,0 +1,58 @@
+package com.agmmps.commons.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.agmmps.commons.R;
+
+
+public class AnuncioFragment extends Fragment {
+
+
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//
+//    private String mParam1;
+//    private String mParam2;
+
+    EditText etAnuncio;
+
+    public AnuncioFragment() {
+
+    }
+
+    public static AnuncioFragment newInstance() {
+        AnuncioFragment fragment = new AnuncioFragment();
+        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_anuncio, container, false);
+
+        etAnuncio = view.findViewById(R.id.etAnuncioFrag);
+
+        return view;
+    }
+}
