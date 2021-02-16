5556a1e9d65a79c2910addc7068a9508f7a1f218
pablosc93 <pablo29.sanchezc@gmail.com>
2021-02-16 18:03:26 +0100

    uuuuu

--- app/src/main/java/com/agmmps/commons/fragments/BusquedaVecinosFragment.java
+++ app/src/main/java/com/agmmps/commons/fragments/BusquedaVecinosFragment.java
@@ -52,7 +52,7 @@ public class BusquedaVecinosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragmentt
         View view = inflater.inflate(R.layout.fragment_busqueda_vecinos, container, false);
 
         etEscribir = view.findViewById(R.id.etVecinosBusqueda);
