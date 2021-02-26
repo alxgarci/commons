2a8266f85f753d244ef6bf503fe0cb02f88a2c69
pablosc93 <pablo29.sanchezc@gmail.com>
2021-02-26 14:03:54 +0100

    k

--- app/src/main/java/com/agmmps/commons/javabeans/Datos.java
+++ app/src/main/java/com/agmmps/commons/javabeans/Datos.java
@@ -11,10 +11,10 @@ public class Datos {
     public Datos() {
         datos = new ArrayList<Anuncio>();
         //fgg
-        datos.add(new Anuncio(1, "Busco amiguitos para salir por ahi", (new Usuario(1, R.drawable.usuario_1, "julia@gmail.com", "123456", "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
-        datos.add(new Anuncio(2, "Busco amiguitas para jugar al ping pong", (new Usuario(2, R.drawable.usuario_2, "mikel@gmail.com", "654321", "Mikel Izubirraga", "Bernabeu", "Soy una chico muy enrollado"))));
-//        datos.add(new Anuncio(3, "Busco amiguitos para salir por ahi", (new Usuario(3, R.drawable.usuario_3, "Julia Ramirez", "Chamartin", "Soy una chica muy guay y me todo eso"))));
-//        datos.add(new Anuncio(4, "Busco amiguitas para jugar al ping pong", (new Usuario(4, R.drawable.usuario_4, "Mikel Izubirraga", "Bernabeu", "Soy una chico muy enrollado"))));
+        datos.add(new Anuncio(1, "Busco amiguitas para jugar al ping pong", (new Usuario(1, R.drawable.usuario_1, "mikel@gmail.com", "654321", "Mikel Izubirraga", "Villaverde", "Soy una chico muy enrollado"))));
+        datos.add(new Anuncio(2, "Busco amiguitos para salir por ahi", (new Usuario(2, R.drawable.usuario_3, "julia@gmail.com", "123456", "Julia Ramirez", "Chamartin", "Soy una chica muy guay y  todo eso"))));
+        datos.add(new Anuncio(3, "Busco amiguitos para salir por ahi", (new Usuario(3, R.drawable.usuario_2, "kevin@gmail.com", "123456a", "Kevin Smith", "Tribunal", "Soy una chica muy guay y me todo eso"))));
+        datos.add(new Anuncio(4, "Busco amiguitas para jugar al ping pong", (new Usuario(4, R.drawable.usuario_4, "jeniffer@gmail.com", "123456b", "Jeniffer Bloom", "Bernabeu", "Soy una chico muy enrollado"))));
 
     }
 
